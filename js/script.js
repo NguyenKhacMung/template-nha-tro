5c67b065f73c6eb2e18f8023eec65fe47f809620
Mung Nguyen <mungdola999@gmail.com>
2021-06-03 22:13:09 +0700

    add template nhatro

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('#hien-mk').on('click',function () {
+var passInput=$('#password-signup').attr('type');			
+	if (passInput == 'password') {
+		$('#password-signup').attr('type', 'text');
+	}
+	if (passInput == 'text') {
+		$('#password-signup').attr('type', 'password');
+	}
+});
+
+$('.taotaikhoan').on('click', function(){
+	$('#sign-in').modal('hide');
+	setTimeout(function(){
+		$('#sign-up').modal('show');}, 500);	
+});
